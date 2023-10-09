b73b48c2e6c74872bddcaccf0e6722184b16a097
Beltrandes Marques <beltsmarques@hotmail.com>
2023-10-09 00:10:17 -0300

    feat: global structure adjustment and form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
   results: [] = []
+  form!: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+      this.form = this.formBuilder.group({
+        hour: '',
+        showClosed: false
+      })
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value)
+  }
+
+  onClean(): void {
+    this.form.reset()
+  }
 }
